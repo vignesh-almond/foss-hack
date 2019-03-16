b56ee99721aa768f0b7ddea930b2fba4415b1fd4
deeplambda <vignesh@almond.ai>
2019-03-16 14:30:29 +0800

    Recorder working

--- recorder/server_foss.js
+++ recorder/server_foss.js
@@ -12,6 +12,18 @@ var port    =   process.env.PORT || 8687;
 //js file paths
 app.use(express.static(__dirname + '/public'));
 
+const admin = require('firebase-admin');
+const serviceAccount = require('./account_key.json');
+
+//Firebase stuff
+// admin.initializeApp(functions.config().firebase);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+var db = admin.firestore();
+
 // viewed at http://localhost:8687/record
 app.get('/record', function(req, res) {
   console.log(__dirname);
